428199519cc3cb0322c93a7f2d52f6c91417a842
follet lars <FolletLars@MacBook-Air-van-follet.local>
2018-02-15 22:52:52 +0100

    Login sorta works
    Sometimes the login is succesful and redirects as it should, other times it does not work and it executes the then and the catch sequentially

--- ideaX/Components/SignInForm.js
+++ ideaX/Components/SignInForm.js
@@ -3,6 +3,15 @@ import { View, Text, Button } from 'react-native';
 import firebase from 'firebase';
 import TextFieldInput from './TextFieldInput';
 import style from '../Assets/style';
+import {
+    StackNavigator,
+} from 'react-navigation';
+
+import HomeScreen from "../Screens/HomeScreen/HomeScreen.js";
+
+const Redirect = StackNavigator({
+    Home: { screen: HomeScreen },
+});
 
 class SignInForm extends Component {
     state = { email: '', password: '', error: '', loading: false };
@@ -10,13 +19,23 @@ class SignInForm extends Component {
         this.setState({ error: '', loading: true });
         const { email, password } = this.state;
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(() => { this.setState({ error: '', loading: false }); })
+            .then(() => {
+                this.setState({ error: '', loading: false });
+                console.log('check');
+                return navigate('Home');
+            })
             .catch(() => {
-                firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .then(() => { this.setState({ error: '', loading: false }); })
-                    .catch(() => {
+                console.log('error');
+                // firebase.auth().createUserWithEmailAndPassword(email, password)
+                //     .then(() => {
+                //         console.log('check2');
+                //         this.setState({ error: '', loading: false });
+                //         return navigate('Home');
+                //     })
+                //     .catch(() => {
+                //         console.log('error2');
                         this.setState({ error: 'Authentication failed.', loading: false });
-                    });
+                    // });
             });
     }
     renderButtonOrLoading() {
